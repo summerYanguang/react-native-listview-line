3731245cbbb3830ecb765bfd71bbe617864b1b9d
yanguang <58027929@qq.com>
2016-06-01 22:03:09 +0800

    m-second

--- index.android.js
+++ index.android.js
@@ -16,7 +16,6 @@ class RNflexbox extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
